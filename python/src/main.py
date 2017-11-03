8849f3138ffa50de6f843db32d0181796707f456
sekimoto <e1456204@soka-u.jp>
2017-11-03 23:54:38 +0900

    Update

--- python/src/main.py
+++ python/src/main.py
@@ -6,9 +6,31 @@ def Weekday_judge(date):
 	weekday=new_date.weekday()
 	return weekday
 	
+#1週間経ったかを判定し、週合計労働時間Total_weekを0にする関数Check_aweek
+def Check_aweek(date, weekday):
+	global Total_week
+	global keep_week_day
+	day = date.split("/")
+	'''
+	weekdayには0~6の整数(月曜日~日曜日)が代入されている。
+	keep_week_dayには最後に読み込んだ曜日と日付が代入されている。
+	'''
+	if keep_week_day[0] >= weekday:
+		Total_week=0
+	elif keep_week_day[1]+7 <= int(day[2]):
+		Total_week=0
+	else:
+		pass
+	keep_week_day[0] = weekday
+	keep_week_day[1] = int(day[2])
+	return 0
+
+#法定内残業時間数(時)、法定外残業時間数(時)、深夜残業時間数(時)、所定休日労働時間数(時)、法定休日労働時間数(時)を計算してmainに渡す関数Calculation
 def Calculation(time, weekday, output_data):
-	over_24oclock = 0
-	total_day = 0
+	global Total_week
+	over_24oclock = 0		#24時以降の労働時間
+	total_day = 0			#1日の労働時間
+	unemployed_time = 0	#不就労働時間
 	
 	time = time.split()
 	for i in range(len(time)):
@@ -18,40 +40,70 @@ def Calculation(time, weekday, output_data):
 		start = time1[0] + time1[1]/60
 		end = time2[0] + time2[1]/60
 		total_day += (end - start)
-		if end > 22.00:			#22時以降の勤務を確認
+		#22時以降の勤務を確認
+		if end > 22.00:	
 			output_data[2] += Night_overtime(start, end)
-			if end >24.00:		#24時以降の勤務を確認
+			#24時以降の勤務を確認
+			if end >24.00:
 				over_24oclock += Check_over24(start, end)
-	
-				
-	if weekday <4:		#月〜木曜日
-		output_data = Check_overtime(total_day, output_data)
-	
-	elif weekday == 4:	#金曜日
-		#24時以降に勤務がある場合
-		if over_24oclock !=0:
-			output_data[3] += over_24oclock
-			total_day -= over_24oclock
-			output_data = Check_overtime(total_day, output_data)
-		#24時以降に勤務がない場合
+		#不就労働時間を確認
+		if (start >= 8.00 and end <= 12.00):
+			unemployed_time += (4.00-(end-start))
+		if (start >= 13.00 and end <= 16.00):
+			unemployed_time += (3.00-(end-start))		
+	Total_week += total_day
+	#月〜木曜日
+	if weekday <4:	
+		#週40時間以内の勤務の場合
+		if Total_week <40:
+			output_data = Check_overtime(total_day, output_data, unemployed_time)
+		#週40時間を超過した勤務の場合
 		else:
-			output_data = Check_overtime(total_day, output_data)
-			
-	elif weekday == 5:	#土曜日
-		#24時以降に勤務がある場合
+			output_data[1] += (Total_week - 40)
+			total_day -= (Total_week - 40)
+			output_data = Check_overtime(total_day, output_data, unemployed_time)
+	#金曜日
+	elif weekday == 4:
+			#24時以降に勤務がある場合
+			if over_24oclock !=0:
+				Total_week -= over_24oclock	#24時以降は所定休日労働時間なので、週の労働時間からは省く
+				#週40時間以内の勤務の場合
+				if Total_week < 40:
+					output_data[3] += over_24oclock
+					total_day -= over_24oclock
+					output_data = Check_overtime(total_day, output_data, unemployed_time)
+				#週40時間を超過した勤務の場合
+				else:
+					output_data[3] += over_24oclock
+					total_day -= over_24oclock
+					output_data[1] += (Total_week - 40)
+					total_day -= (Total_week - 40)	
+					output_data = Check_overtime(total_day, output_data, unemployed_time)			
+			#24時以降に勤務がない場合
+			else:
+				#週40時間以内の勤務の場合
+				if Total_week <40:
+					output_data = Check_overtime(total_day, output_data, unemployed_time)
+				#週40時間を超過した勤務の場合
+				else:
+					output_data[1] += (Total_week - 40)
+					total_day -= (Total_week - 40)
+					output_data = Check_overtime(total_day, output_data, unemployed_time)
+	#土曜日
+	elif weekday == 5:
+		#24時以降に勤務がある場合
 		if over_24oclock !=0:
 			output_data[4] += over_24oclock
 			output_data[3] += (total_day - over_24oclock)
-		#24時以降に勤務がない場合
+		#24時以降に勤務がない場合
 		else:
 			output_data[3] += total_day
-		
-	elif weekday == 6: 	#日曜日
+	#日曜日
+	elif weekday == 6: 
 		output_data[4] += total_day
 	
-	#print(output_data,"    ",total_day)
-	
 	return output_data
+
 
 #深夜残業時間数を確認する関数Night_overtime
 def Night_overtime(start, end):
@@ -64,75 +116,115 @@ def Night_overtime(start, end):
 		total_night += (end - start)
 	return total_night
 
+
 #24時以降の勤務時間を確認する関数Check_over24
 def Check_over24(start, end):
 	total_over24=0
 	#開始時刻が24時以前の場合
 	if start <24.00:		
-		total_over24 += (end-22.00)
+		total_over24 += (end-24.00)
 	#開始時刻が24時以降の場合
 	else:
 		total_over24 += (end - start)
 	return total_over24
 
+
 #法定内/法定外残業時間を確認する関数Check_overtime
-def Check_overtime(total, output_data):
+def Check_overtime(total, output_data, unemployed):
 	#勤務時間が7時間以上8時間以内の場合
-	if total >7.00 and total < 8.00:
-		output_data[0] += (total - 7.00)
+	if total >7.00 and total <= 8.00:
+		output_data[0] += (total - 7.00 + unemployed)
 	#勤務時間が8時間以上の場合
 	elif total > 8.00:
-		output_data[0] += 1.00
+		output_data[0] += (1.00+unemployed)
 		output_data[1] += (total - 8.00)
-	return output_data
-	
-def Get_monthend(date):
-	d=date.split("/")
-	year=int(d[0])
-	month=int(d[1])
-	if month==2:
-		if year%4==0:
-			month_end= date+"/29"
-			return month_end
-		month_end= date+"/28"
-		return month_end
-	elif month==1 or month==3 or month==5 or month==7 or month==8 or month==10 or month==12:
-		month_end= date+"/31"
-		return month_end
+	#勤務時間が7時間未満で不就労働時間がある場合
+	elif total <= 7.00 and unemployed!=0:
+		output_data[0] += unemployed
 	else:
-		month_end= date+"/30"
-		return month_end
+		pass
+	return output_data
+
+#前の月のYYYY/MM文字列を作成する関数Mk_last_month
+def Mk_last_month(date):
+	global Check_bug
+	a = date.split("/")
+	a[1] = int(a[1])
+	if a[1] == 1:
+		last_month = a[0]+"/12"
+	
+	elif a[1] <= 12:
+		a[1] = str(a[1]-1)
+		last_month = a[0]+"/"+a[1].zfill(2)
+	else:
+		Check_bug = False
+	return last_month
+
+#前の月の労働時間を計算する関数Count_LMWT
+def Count_LMWT(time):
+	global Total_week
+	total = 0
+	time = time.split()
+	for i in range(len(time)):
+		time[i] = time[i].split("-")
+		time1 = list(map(float, time[i][0].split(":")))
+		time2 = list(map(float, time[i][1].split(":")))
+		start = time1[0] + time1[1]/60
+		end = time2[0] + time2[1]/60
+		total += (end - start)
+	Total_week += total
+	return 0
 
 
+Total_week=0				#週合計労働時間
+keep_week_day=[0, 0]		#最後に読み込んだ[曜日,日付]
+Check_bug=True			#バグ
 def main():
 	import sys
-	input_data=[]
+	global Check_bug
+	global keep_week_day
 	data_line={}
 	#入力データの読み込み
-	input_data.append(input())
-	month_end = Get_monthend(input_data[0])
-	i=1
-	while True:
-		input_data.append(input())
-		data_line[i] = input_data[i].strip("\n")
-		data_line[i] = data_line[i].split(" ",1)
-		if data_line[i][0]==month_end:
-			break
-		i+=1
-	#num_dataにデータの数を代入
-	num_data = i+1
+	try:
+		a=input().strip("\n")
+		data_line[0] = a
+		i=1
+		while True:
+			a=input().strip("\n")
+			data_line[i] = a.split(" ",1)
+			i+=1
+	except EOFError:
+		pass
 
 	#output_data : 法定内残業時間数(時)、法定外残業時間数(時)、深夜残業時間数(時)、所定休日労働時間数(時)、法定休日労働時間数(時)
 	output_data=[0, 0, 0, 0, 0]
+	now_month = data_line[0]
+	last_month=Mk_last_month(now_month)
 	i=1
-	while i<num_data:
-		if input_data[0] in data_line[i][0]:
-			weekday=Weekday_judge(data_line[i][0])
+	while i<len(data_line):
+		weekday=Weekday_judge(data_line[i][0])
+		#対象月の場合
+		if now_month in data_line[i][0]:
+			Check_aweek(data_line[i][0], weekday)
 			output_data = Calculation(data_line[i][1], weekday, output_data)
+		#前月だった場合、週合計労働時間のみ取得する
+		elif last_month in data_line[i][0]:
+			keep_week_day[0] = weekday
+			Count_LMWT(data_line[i][1])
+		else:
+			Check_bug=False
 		i += 1
-
+	
+	#計算結果の出力
 	for i in range(len(output_data)):
 		print(round(output_data[i]))
+	
+	#終了コードの設定
+	if Check_bug:
+		sys.exit(0)
+	else:
+		sys.exit(1)
+
 	
 if __name__ == "__main__":
 	main()
